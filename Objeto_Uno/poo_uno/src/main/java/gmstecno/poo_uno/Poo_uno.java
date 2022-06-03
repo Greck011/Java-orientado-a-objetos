2b7014541fea9eb761bc69c7c85d71b5099f3aef
Greck011 <71396160+Greck011@users.noreply.github.com>
2022-06-03 17:49:33 -0600

    Add files via upload

---
+++ Objeto_Uno/poo_uno/src/main/java/gmstecno/poo_uno/Poo_uno.java
@@ -0,0 +1,16 @@
+package gmstecno.poo_uno;
+
+/*
+ * @author greivin
+ */
+
+public class Poo_uno 
+{
+    public static void main(String[] args) 
+    {
+        auto isuzu = new auto();
+        isuzu.acelerar();
+        System.out.println("////////////");
+        isuzu.velocidad(500);
+    }
+}
---
+++ Objeto_Uno/poo_uno/src/main/java/gmstecno/poo_uno/auto.java
@@ -0,0 +1,18 @@
+/*
+ * @author greivin
+ */
+package gmstecno.poo_uno;
+
+public class auto 
+{
+    //método.
+    public void acelerar() 
+    {
+        System.out.println(" Acelerando......");
+    }
+
+    public void velocidad(int vMax) 
+    {
+        System.out.println(" La velocidad maxima es: " + vMax + " Km/h.");
+    }
+}
